3b7c9d9a6b67664805c43299ed66ec2f290b61f6
Jared Hanson <jaredhanson@gmail.com>
2017-03-15 11:23:39 -0700

    Update tests.

--- test/package.test.js
+++ test/package.test.js
@@ -4,10 +4,11 @@ var pkg = require('..');
 var expect = require('chai').expect;
 
 
-describe('tokens-oz', function() {
+describe('tokens-iron', function() {
   
-  it('should export hello world', function() {
-    expect(pkg.hello).to.equal('world');
+  it('should export functions', function() {
+    expect(pkg.seal).to.be.a('function');
+    expect(pkg.unseal).to.be.a('function')
   });
   
 });
